d629a0f6da005d5f265be6b9731d644f4b693834
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-08-31 18:20:00 +0200

    Initial commit

---
+++ src/main/java/io/hb3nce04/career/backend/CareerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.hb3nce04.career.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hb3nce04/career/backend/CareerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hb3nce04.career.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
